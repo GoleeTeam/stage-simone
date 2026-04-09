748a807091ce000430d399fd864a883ec3d3ca4b
simone <simone@golee.it>
2026-04-09 12:07:11 +0200

    install npm

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-const {readFile, readFileSync} = require('fs');
-const txt = readFileSync('./sas.txt','utf8');
+//const {readFile, readFileSync} = require('fs');
+//const txt = readFileSync('./sas.txt','utf8');
 
-readFile('./sas.txt','utf8', (err,txt)=>{
-    console.log(txt)
-});
+const mymodule = require('./mymodule');
 
-console.log("eeeee");
+console.log(mymodule);
