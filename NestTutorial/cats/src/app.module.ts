8e0c4b64c4386979155378a38ab5ec6895c8c03c
simone <simone@golee.it>
2026-05-07 11:06:07 +0200

    feat(cats): add the docker-compose.yml and the dockerfile, change main for listen all the ip

--- NestTutorial/cats/src/app.module.ts
+++ NestTutorial/cats/src/app.module.ts
@@ -1,11 +1,16 @@
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
-import { Module }from'@nestjs/common';
-import {MongooseModule }from'@nestjs/mongoose';
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [CatsModule, MongooseModule.forRoot('mongodb://localhost:27017/nome-db',)],
+  imports: [
+    CatsModule,
+    MongooseModule.forRoot(
+      process.env.MONGO_URI ?? 'mongodb://localhost:27017/nome-db',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
